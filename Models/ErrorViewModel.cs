5608f684d53a114c6839c6bf5907568d0e97c316
Mel <melhernandez236@gmail.com>
2021-06-01 09:52:32 -0500

    second commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hogwarts_16301106_17100150.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
